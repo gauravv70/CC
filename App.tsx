13eda86d20dda533954140d52849896735b225d4
Gaurav Budhiraja <gauravbudhiraja70@gmail.com>
2022-05-07 03:17:19 +0530

    First screen proto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  SafeAreaView,
+  Button,
+} from "react-native";
+import Line from "./utils/line";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.textHead}> Hi There! {`\n\n`}Welcome to the</Text>
+        <Text style={styles.textHeadLogo}>Class Companion</Text>
+      </View>
+
+      <View style={styles.main}>
+        <Text style={styles.textMain}>New User? {`\n`}</Text>
+        <View style={styles.buttonViewWrapper}>
+          <View style={styles.buttonView}>
+            <Button title="Sign Up"></Button>
+          </View>
+        </View>
+        {/* <Text> {`\n`}</Text> */}
+        <Line></Line>
+        <Text style={styles.textMain}>
+          {`\n`}Already a Companion? {`\n`}
+        </Text>
+        <View style={styles.buttonViewWrapper}>
+          <View style={styles.buttonView}>
+            <Button title="Login"></Button>
+          </View>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E5E5E5",
+  },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#293241",
+    width: "100%",
+    height: "35%",
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+  },
+  textHead: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#FFF",
+  },
+  textHeadLogo: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#EE6C4D",
+  },
+
+  main: {
+    justifyContent: "center",
+    height: "55%",
+    width: "80%",
+  },
+  textMain: {
+    width: "90%",
+    textAlign: "left",
+    color: "#293241",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  buttonViewWrapper: {
+    alignItems: "flex-end",
+  },
+  buttonView: {
+    width: "50%",
+  },
+
+  footer: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: "10%",
+    width: "100%",
+    backgroundColor: "#EE6C4D",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    // position: "relative",
   },
 });
