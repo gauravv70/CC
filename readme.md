ac6f22043dc61800af31b45b0ee2f55738b822b6
Gaurav Budhiraja <gauravbudhiraja70@gmail.com>
2022-05-07 23:48:31 +0530

    Added docs

---
+++ readme.md
@@ -0,0 +1,10 @@
+Install node version between 12 and 16
+
+Install expo globally
+npm i -g expo-cli
+
+npm install
+
+npm start
+
+download expo on android or ios -> login -> scan the QR code on built address
---
+++ screens/SignIn.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function SignIn() {
+  return (
+    <View
+      style={{
+        marginTop: 20,
+        height: 2,
+        backgroundColor: "#293241",
+        alignSelf: "stretch",
+      }}
+    />
+  );
+}
